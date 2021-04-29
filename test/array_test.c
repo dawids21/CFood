5cc7fe4ba425b04957bdecbbd2d9b2ecdc5e8887
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-29 20:42:02 +0200

    create file for array test

---
+++ test/array_test.c
@@ -0,0 +1,19 @@
+//
+// Created by dawids21 on 4/29/21.
+//
+
+#include <unity.h>
+
+void setUp() {
+
+}
+
+void tearDown() {
+
+}
+
+int main(void) {
+    UNITY_BEGIN();
+
+    return UNITY_END();
+}
