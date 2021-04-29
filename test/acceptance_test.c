2f25273b1f7548922ae55dbe0160ec7ceadd4ccf
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-29 07:29:13 +0200

    create basic file for acceptance test

--- test/acceptance_test.c
+++ test/acceptance_test.c
@@ -2,3 +2,24 @@
 // Created by dawids21 on 4/29/21.
 //
 
+#include <unity.h>
+
+void setUp() {
+
+}
+
+void tearDown() {
+
+}
+
+void acceptance_test(void) {
+    TEST_IGNORE_MESSAGE("Acceptance test not implemented yet");
+}
+
+int main(void) {
+    UNITY_BEGIN();
+
+    RUN_TEST(acceptance_test);
+
+    return UNITY_END();
+}
