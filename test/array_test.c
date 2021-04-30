048b75a35aa84255d423c559f2aa5f0114b9628a
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-30 10:53:40 +0200

    write full test for adding items

--- test/array_test.c
+++ test/array_test.c
@@ -3,6 +3,7 @@
 //
 
 #include <unity.h>
+#include <array.h>
 
 void setUp() {
 
@@ -12,8 +13,31 @@ void tearDown() {
 
 }
 
+void test_when_added_two_items_return_two_items() {
+    Array array = new_array(INT);
+
+    append(array, (ArrayItem) 5);
+    append(array, (ArrayItem) 8);
+    ArrayItem *items = get_all_items(array);
+
+    TEST_ASSERT_EQUAL(5, items[0].int_item);
+    TEST_ASSERT_EQUAL(8, items[1].int_item);
+
+    delete_array(array);
+
+    TEST_FAIL();
+}
+
+
+void test_when_added_three_items_return_size_of_three() {
+
+}
+
 int main(void) {
     UNITY_BEGIN();
+
+    RUN_TEST(test_when_added_two_items_return_two_items);
+    RUN_TEST(test_when_added_three_items_return_size_of_three);
 
     return UNITY_END();
 }
