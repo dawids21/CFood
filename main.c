801863d93f82cac9422d3e454b979a5e8ce205c4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-03 19:13:54 +0200

    handle options in main menu

--- main.c
+++ main.c
@@ -1,23 +1,50 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <ingredient_service.h>
+
+static void input_char(char *dest);
+
+static void manage_ingredients_handler(IngredientService ingredient_service);
 
 int main() {
     IngredientService ingredient_service = new_ingredient_service();
 
-    printf("***** CFood *****\n");
-    printf("-----------------\n");
-    printf("1. Manage ingredients");
-    printf("2. Exit program");
+    while (true) {
+        printf("***** CFood *****\n");
+        printf("-----------------\n");
+        printf("1. Manage ingredients\n");
+        printf("2. Exit program\n");
+        printf("Choose option: ");
+        char option;
+        input_char(&option);
+        if (option == '1') {
+            manage_ingredients_handler(ingredient_service);
+        } else if (option == '2') {
+            break;
+        } else {
+            printf("Unknown option");
+        }
+    }
 
     delete_ingredient_service(ingredient_service);
     return 0;
 }
 
+static void input_char(char *dest) {
+    char input[3];
+    do {
+        fgets(input, 3, stdin);
+    } while (input[0] == '\n');
+
+    *dest = input[0];
+}
+
 static void manage_ingredients_handler(IngredientService ingredient_service) {
     printf("***** CFood *****\n");
     printf("-----------------\n");
-    printf("1. List all ingredients");
-    printf("2. Add new ingredient");
-    printf("3. Modify existing ingredient");
-    printf("4. Delete existing ingredient");
+    printf("1. List all ingredients\n");
+    printf("2. Add new ingredient\n");
+    printf("3. Modify existing ingredient\n");
+    printf("4. Delete existing ingredient\n");
+    printf("Choose option: ");
 }
