31d081882a308de28e0678aedbd2dd39d49687a2
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-03 13:15:06 +0200

    function to return name and amount of ingredient

--- ingredient/ingredient.c
+++ ingredient/ingredient.c
@@ -2,6 +2,7 @@
 // Created by dawids21 on 4/28/21.
 //
 
+#include <string.h>
 #include "ingredient.h"
 
 typedef enum {
@@ -15,3 +16,19 @@ struct Ingredient {
     int amount;
     IngredientType type;
 };
+
+bool get_name(Ingredient ingredient, char *result, int result_len) {
+    if (ingredient == NULL) {
+        return false;
+    }
+    strncpy(result, ingredient->name, result_len);
+    return true;
+}
+
+bool get_amount(Ingredient ingredient, int *result) {
+    if (ingredient == NULL) {
+        return false;
+    }
+    *result = ingredient->amount;
+    return true;
+}
--- ingredient/ingredient.h
+++ ingredient/ingredient.h
@@ -6,7 +6,12 @@
 #define CFOOD_INGREDIENT_H
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct Ingredient *Ingredient;
+
+bool get_name(Ingredient ingredient, char *result, int result_len);
+
+bool get_amount(Ingredient ingredient, int *result);
 
 #endif //CFOOD_INGREDIENT_H
