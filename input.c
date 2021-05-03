2298a6dc9a26f06c35d7aeaf2c54563720280664
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-03 21:00:32 +0200

    function for input integer

--- input.c
+++ input.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "input.h"
 
 void input_char(char *dest) {
@@ -21,4 +22,15 @@ void input_string(char *dest, int limit) {
     if ((strlen(dest) > 0) && (dest[strlen(dest) - 1] == '\n')) {
         dest[strlen(dest) - 1] = '\0';
     }
+}
+
+void input_integer(int *dest) {
+    char input[15];
+    fgets(input, 15, stdin);
+
+    if ((strlen(input) > 0) && (dest[strlen(input) - 1] == '\n')) {
+        dest[strlen(input) - 1] = '\0';
+    }
+
+    *dest = (int) strtol(input, NULL, 10);
 }
--- input.h
+++ input.h
@@ -9,4 +9,6 @@ void input_char(char *dest);
 
 void input_string(char *dest, int limit);
 
+void input_integer(int *dest);
+
 #endif //CFOOD_INPUT_H
