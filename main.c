0e9d454a7e86615f4c3b08442d8bba068d0865d4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-03 18:31:17 +0200

    display basic menu

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
-#include <ingredient.h>
+#include <ingredient_service.h>
 
 int main() {
-    printf("Hello, World!\n");
+    IngredientService ingredient_service = new_ingredient_service();
+
+    printf("***** CFood *****\n");
+    printf("-----------------\n");
+    printf("1. Manage ingredients");
+    printf("2. Exit program");
+
+    delete_ingredient_service(ingredient_service);
     return 0;
+}
+
+static void manage_ingredients_handler(IngredientService ingredient_service) {
+    printf("***** CFood *****\n");
+    printf("-----------------\n");
+    printf("1. List all ingredients");
+    printf("2. Add new ingredient");
+    printf("3. Modify existing ingredient");
+    printf("4. Delete existing ingredient");
 }
