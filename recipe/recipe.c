d8f3fdbf6e0f520af192f37be28f05492ae75150
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-07 17:06:52 +0200

    create recipe source files

---
+++ recipe/recipe.c
@@ -0,0 +1,5 @@
+//
+// Created by dawids21 on 5/7/21.
+//
+
+#include "recipe.h"
---
+++ recipe/recipe.h
@@ -0,0 +1,8 @@
+//
+// Created by dawids21 on 5/7/21.
+//
+
+#ifndef CFOOD_RECIPE_H
+#define CFOOD_RECIPE_H
+
+#endif //CFOOD_RECIPE_H
