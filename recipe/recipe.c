3188c867a31ea119d131fdf18ebb13eb546a26d0
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-07 18:33:25 +0200

    write function to create recipe

--- recipe/recipe.c
+++ recipe/recipe.c
@@ -2,6 +2,8 @@
 // Created by dawids21 on 5/7/21.
 //
 
+#include <malloc.h>
+#include <string.h>
 #include "recipe.h"
 
 struct Recipe {
@@ -12,3 +14,30 @@ struct Recipe {
     int *ingredients;
     int num_of_ingredients;
 };
+
+Recipe
+create_new_recipe(int id, char *name, char *steps[], int num_of_steps, int ingredients[], int num_of_ingredients) {
+    Recipe recipe = malloc(sizeof(struct Recipe));
+
+    recipe->id = id;
+
+    recipe->name = malloc((strlen(name) + 1) * sizeof(char));
+    strcpy(recipe->name, name);
+
+    recipe->steps = malloc(sizeof(char *) * num_of_steps);
+    for (int i = 0; i < num_of_steps; ++i) {
+        recipe->steps[i] = malloc((strlen(steps[i]) + 1) * sizeof(char));
+        strcpy(recipe->steps[i], steps[i]);
+    }
+
+    recipe->num_of_steps = num_of_steps;
+
+    recipe->ingredients = malloc(num_of_ingredients * sizeof(int));
+    for (int i = 0; i < num_of_ingredients; ++i) {
+        recipe->ingredients[i] = ingredients[i];
+    }
+
+    recipe->num_of_ingredients = num_of_ingredients;
+
+    return recipe;
+}
