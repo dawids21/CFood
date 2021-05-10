c9832015198ea042455ee4e7b95996691b3a7e04
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-10 14:14:23 +0200

    handle list and add recipe options

--- recipe_service_controller.c
+++ recipe_service_controller.c
@@ -42,11 +42,57 @@ void recipe_service_handle_option(char option, RecipeService service, Ingredient
 }
 
 static void list_recipes(RecipeService service) {
-
+    int num_of_recipes = get_num_of_recipes(service);
+    if (num_of_recipes == 0) {
+        printf("No recipes\n");
+        return;
+    }
+    RecipeReadModel recipes[num_of_recipes];
+    get_all_recipes(service, recipes);
+    printf("ID | Name | Is possible?\n");
+    printf("------------------\n");
+    for (int i = 0; i < num_of_recipes; i++) {
+        RecipeReadModel current = recipes[i];
+        printf("%d | %s | %s\n", current.id, current.name, current.is_possible ? "YES" : "NO");
+    }
 }
 
-static void add_new_recipe(RecipeService service) {
+static void add_new_recipe(RecipeService service, IngredientService ingredient_service) {
+    char name[100];
+    printf("Name: ");
+    input_string(name, 100);
 
+    int num_of_steps;
+    printf("Num of steps: ");
+    input_integer(&num_of_steps);
+
+    char *steps[num_of_steps];
+    for (int i = 0; i < num_of_steps; ++i) {
+        steps[i] = malloc(sizeof(char) * 200);
+        printf("Step %d: ", i);
+        input_string(steps[i], 200);
+    }
+
+    ingredient_service_handle_option('1', ingredient_service);
+    int num_of_ingredients;
+    printf("Num of ingredients: ");
+    input_integer(&num_of_ingredients);
+
+    RecipeIngredient ingredients[num_of_ingredients];
+    for (int i = 0; i < num_of_ingredients; ++i) {
+        ingredients[i] = malloc(sizeof(struct RecipeIngredient));
+        printf("Ingredient %d id: ", i);
+        input_integer(&ingredients[i]->id);
+        printf("Ingredient %d amount: ", i);
+        input_integer(&ingredients[i]->amount);
+    }
+
+    bool success = add_recipe(service, name, steps, num_of_steps, ingredients, num_of_ingredients);
+    if (success) {
+        printf("Recipe added\n");
+    } else {
+        printf("Problem with adding recipe\n");
+    }
 }
 
 static void delete_existing_recipe(RecipeService service) {
