3cca3fb6e291c76bda3285dcf871b0233e86f49e
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-11 11:24:10 +0200

    create menu for recommendation module

--- recommendation_service_controller.c
+++ recommendation_service_controller.c
@@ -5,7 +5,12 @@
 #include "recommendation_service_controller.h"
 
 void recommendation_service_display_main_menu() {
-
+    printf("***** CFood *****\n");
+    printf("-----------------\n");
+    printf("1. Get recommendations\n");
+    printf("2. Try something new (not implemented yet)\n");
+    printf("3. Return\n");
+    printf("Choose option: ");
 }
 
 void recommendation_service_handle_option(char option, RecommendationService service) {
