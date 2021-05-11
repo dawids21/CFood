cf9c768b3a828ef8745d51f013bb0e8586d4de3d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-11 09:34:25 +0200

    add functions for creating and deleting RecommendationService

--- recommendation/recommendation_service.c
+++ recommendation/recommendation_service.c
@@ -2,11 +2,20 @@
 // Created by dawids21 on 5/11/21.
 //
 
-#include <recipe_service.h>
-#include <ingredient_service.h>
 #include "recommendation_service.h"
 
 struct RecommendationService {
     RecipeService recipe_service;
     IngredientService ingredient_service;
 };
+
+RecommendationService new_recommendation_service(RecipeService recipe_service, IngredientService ingredient_service) {
+    RecommendationService service = malloc(sizeof(struct RecommendationService));
+    service->recipe_service = recipe_service;
+    service->ingredient_service = ingredient_service;
+    return service;
+}
+
+void delete_recommendation_service(RecipeService service) {
+    free(service);
+}
--- recommendation/recommendation_service.h
+++ recommendation/recommendation_service.h
@@ -5,6 +5,13 @@
 #ifndef CFOOD_RECOMMENDATION_SERVICE_H
 #define CFOOD_RECOMMENDATION_SERVICE_H
 
+#include <recipe_service.h>
+#include <ingredient_service.h>
+
 typedef struct RecommendationService *RecommendationService;
+
+RecommendationService new_recommendation_service(RecipeService recipe_service, IngredientService ingredient_service);
+
+void delete_recommendation_service(RecipeService service);
 
 #endif //CFOOD_RECOMMENDATION_SERVICE_H
