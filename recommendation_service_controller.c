f333b35567c7f46a646166b09026275b74980279
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-11 11:44:51 +0200

    basic structure for recommendtation menu

--- recommendation_service_controller.c
+++ recommendation_service_controller.c
@@ -3,10 +3,13 @@
 //
 
 #include "recommendation_service_controller.h"
+#include "input.h"
 
 static void get_recommendations(RecommendationService service);
 
 static void try_something_new(RecommendationService service);
+
+static void print_recommendation_menu();
 
 void recommendation_service_display_main_menu() {
     printf("***** CFood *****\n");
@@ -33,9 +36,46 @@ void recommendation_service_handle_option(char option, RecommendationService ser
 }
 
 static void get_recommendations(RecommendationService service) {
-
+    int num_of_available_recipes = get_number_of_available_recipes(service);
+    if (num_of_available_recipes <= 0) {
+        printf("No available recipes");
+        return;
+    }
+    int id_recipes[num_of_available_recipes];
+    get_available_recipes(service, id_recipes, num_of_available_recipes);
+    int current = 0;
+    int run = 1;
+    while (run) {
+        char option;
+        input_char(&option);
+        print_recommendation_menu();
+        switch (option) {
+            case '1':
+                //TODO
+                run = 0;
+                break;
+            case '2':
+                current = (current + 1) % num_of_available_recipes;
+                break;
+            case '3':
+                run = 0;
+                break;
+            default:
+                printf("Unknown option\n");
+                break;
+        }
+    }
 }
 
 static void try_something_new(RecommendationService service) {
     //TODO
+}
+
+static void print_recommendation_menu() {
+    printf("***** CFood *****\n");
+    printf("-----------------\n");
+    printf("1. Prepare meal (not implemented yet)\n");
+    printf("2. Next (not implemented yet)\n");
+    printf("3. Return\n");
+    printf("Choose option: ");
 }
