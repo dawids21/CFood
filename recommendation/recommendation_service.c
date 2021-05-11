b5b948574e4d66ab9e1e52223af81328699a6124
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-11 09:31:22 +0200

    create struct for recommendation service

--- recommendation/recommendation_service.c
+++ recommendation/recommendation_service.c
@@ -2,4 +2,11 @@
 // Created by dawids21 on 5/11/21.
 //
 
+#include <recipe_service.h>
+#include <ingredient_service.h>
 #include "recommendation_service.h"
+
+struct RecommendationService {
+    RecipeService recipe_service;
+    IngredientService ingredient_service;
+};
--- recommendation/recommendation_service.h
+++ recommendation/recommendation_service.h
@@ -5,4 +5,6 @@
 #ifndef CFOOD_RECOMMENDATION_SERVICE_H
 #define CFOOD_RECOMMENDATION_SERVICE_H
 
+typedef struct RecommendationService *RecommendationService;
+
 #endif //CFOOD_RECOMMENDATION_SERVICE_H
