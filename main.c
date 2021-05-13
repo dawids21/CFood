b05c444844b55a728268a9053b613b437b234665
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-13 18:20:44 +0200

    link all callbacks in the code

--- main.c
+++ main.c
@@ -126,60 +126,7 @@ static void recommendation_handler(RecommendationService recommendation_service,
     }
 }
 
-static void main_gtk(int argc, char *argv[]) {
-
-    App *app = g_slice_new(App);
-    if (access(INGREDIENT_SERVICE_FILENAME, F_OK) == 0) {
-        app->ingredient_service = restore_ingredient_service(INGREDIENT_SERVICE_FILENAME);
-    } else {
-        app->ingredient_service = new_ingredient_service(INGREDIENT_SERVICE_FILENAME);
-    }
-
-    if (access(RECIPE_SERVICE_FILENAME, F_OK) == 0) {
-        app->recipe_service = restore_recipe_service(RECIPE_SERVICE_FILENAME, app->ingredient_service);
-    } else {
-        app->recipe_service = new_recipe_service(RECIPE_SERVICE_FILENAME, app->ingredient_service);
-    }
-
-    app->recommendation_service = new_recommendation_service(app->recipe_service, app->ingredient_service);
-
-    GtkBuilder *builder;
-    GtkWidget *window;
-
-    gtk_init(&argc, &argv);
-
-    builder = gtk_builder_new_from_file("window_main.glade");
-
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
-    gtk_builder_connect_signals(builder, app);
-
-    app->stack_main = GTK_STACK(gtk_builder_get_object(builder, "stack_main"));
-    app->stack_recommendations = GTK_STACK(gtk_builder_get_object(builder, "stack_recommendations"));
-    app->lbl_get_recommendations_recipe_name = GTK_LABEL(
-            gtk_builder_get_object(builder, "lbl_get_recommendations_recipe_name"));
-    app->lbl_get_recommendations_num_of_uses = GTK_LABEL(
-            gtk_builder_get_object(builder, "lbl_get_recommendations_num_of_uses"));
-    app->list_get_recommendations_ingredients = GTK_LIST_BOX(
-            gtk_builder_get_object(builder, "list_get_recommendations_ingredients"));
-    app->list_get_recommendations_steps = GTK_LIST_BOX(
-            gtk_builder_get_object(builder, "list_get_recommendations_steps"));
-    app->lbl_try_something_new_recipe_name = GTK_LABEL(
-            gtk_builder_get_object(builder, "lbl_try_something_new_recipe_name"));
-    app->list_try_something_new_ingredients = GTK_LIST_BOX(
-            gtk_builder_get_object(builder, "list_try_something_new_ingredients"));
-    app->list_try_something_new_steps = GTK_LIST_BOX(gtk_builder_get_object(builder, "list_try_something_new_steps"));
-
-    app->current_recommendation_index = 0;
-
-    g_object_unref(builder);
-
-    gtk_widget_show(window);
-    gtk_main();
-
-    g_slice_free(App, app);
-}
-
-void on_window_main_destroy(GtkWidget *widget, App *app) {
+static void on_window_main_destroy(GtkWidget *widget, App *app) {
     delete_recommendation_service(app->recommendation_service);
 
     save_recipe_service(app->recipe_service);
@@ -205,30 +152,145 @@ void on_btn_get_recommendations_next_clicked(GtkButton *button, App *app) {
 
 }
 
-void on_btn_get_recommendations_prepare_clicked(GtkButton *button, App *app) {
+static void on_btn_get_recommendations_prepare_clicked(GtkButton *button, App *app) {
 
 }
 
-void on_btn_main_stack_recipes_clicked(GtkButton *button, App *app) {
+static void on_btn_main_stack_recipes_clicked(GtkButton *button, App *app) {
 
 }
 
-void on_btn_main_stack_ingredients_clicked(GtkButton *button, App *app) {
+static void on_btn_main_stack_ingredients_clicked(GtkButton *button, App *app) {
 
 }
 
-void on_btn_main_stack_recommendations_clicked(GtkButton *button, App *app) {
+static void on_btn_main_stack_recommendations_clicked(GtkButton *button, App *app) {
     gtk_stack_set_visible_child_name(app->stack_main, "recommendation_stack");
 }
 
-void on_btn_stack_recommendations_get_recommendations_clicked(GtkButton *button, App *app) {
+static void on_btn_stack_recommendations_get_recommendations_clicked(GtkButton *button, App *app) {
     app->current_recommendation_index = 0;
     recommendation_service_display_recipe_get_recommendations(app->current_recommendation_index, app);
     gtk_stack_set_visible_child_name(app->stack_recommendations, "get_recommendations");
 }
 
-void on_btn_stack_recommendations_try_something_new_clicked(GtkButton *button, App *app) {
+static void on_btn_stack_recommendations_try_something_new_clicked(GtkButton *button, App *app) {
     app->current_recommendation_index = 0;
     recommendation_service_display_recipe_try_something_new(app->current_recommendation_index, app);
     gtk_stack_set_visible_child_name(app->stack_recommendations, "try_something_new");
+}
+
+static void on_btn_modify_ingredient_add_clicked(GtkButton *button, App *app) {
+
+}
+
+static void on_btn_modify_ingredient_cancel_clicked(GtkButton *button, App *app) {
+
+}
+
+static void on_btn_add_ingredient_add_clicked(GtkButton *button, App *app) {
+
+}
+
+static void on_btn_add_ingredient_cancel_clicked(GtkButton *button, App *app) {
+
+}
+
+static void on_btn_ingredients_list_delete_clicked(GtkButton *button, App *app) {
+
+}
+
+static void on_btn_ingredients_list_modify_clicked(GtkButton *button, App *app) {
+
+}
+
+static void on_btn_ingredients_list_add_clicked(GtkButton *button, App *app) {
+
+}
+
+static void main_gtk(int argc, char *argv[]) {
+
+    App *app = g_slice_new(App);
+    if (access(INGREDIENT_SERVICE_FILENAME, F_OK) == 0) {
+        app->ingredient_service = restore_ingredient_service(INGREDIENT_SERVICE_FILENAME);
+    } else {
+        app->ingredient_service = new_ingredient_service(INGREDIENT_SERVICE_FILENAME);
+    }
+
+    if (access(RECIPE_SERVICE_FILENAME, F_OK) == 0) {
+        app->recipe_service = restore_recipe_service(RECIPE_SERVICE_FILENAME, app->ingredient_service);
+    } else {
+        app->recipe_service = new_recipe_service(RECIPE_SERVICE_FILENAME, app->ingredient_service);
+    }
+
+    app->recommendation_service = new_recommendation_service(app->recipe_service, app->ingredient_service);
+
+    GtkBuilder *builder;
+    GtkWidget *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new_from_file("window_main.glade");
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+
+    app->stack_main = GTK_STACK(gtk_builder_get_object(builder, "stack_main"));
+    app->stack_recommendations = GTK_STACK(gtk_builder_get_object(builder, "stack_recommendations"));
+    app->lbl_get_recommendations_recipe_name = GTK_LABEL(
+            gtk_builder_get_object(builder, "lbl_get_recommendations_recipe_name"));
+    app->lbl_get_recommendations_num_of_uses = GTK_LABEL(
+            gtk_builder_get_object(builder, "lbl_get_recommendations_num_of_uses"));
+    app->list_get_recommendations_ingredients = GTK_LIST_BOX(
+            gtk_builder_get_object(builder, "list_get_recommendations_ingredients"));
+    app->list_get_recommendations_steps = GTK_LIST_BOX(
+            gtk_builder_get_object(builder, "list_get_recommendations_steps"));
+    app->lbl_try_something_new_recipe_name = GTK_LABEL(
+            gtk_builder_get_object(builder, "lbl_try_something_new_recipe_name"));
+    app->list_try_something_new_ingredients = GTK_LIST_BOX(
+            gtk_builder_get_object(builder, "list_try_something_new_ingredients"));
+    app->list_try_something_new_steps = GTK_LIST_BOX(gtk_builder_get_object(builder, "list_try_something_new_steps"));
+
+    app->current_recommendation_index = 0;
+
+    gtk_builder_add_callback_symbol(builder, "on_window_main_destroy", G_CALLBACK(on_window_main_destroy));
+    gtk_builder_add_callback_symbol(builder, "on_btn_try_something_new_next_clicked",
+                                    G_CALLBACK(on_btn_try_something_new_next_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_try_something_new_prepare_clicked",
+                                    G_CALLBACK(on_btn_try_something_new_prepare_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_get_recommendations_next_clicked",
+                                    G_CALLBACK(on_btn_get_recommendations_next_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_get_recommendations_prepare_clicked",
+                                    G_CALLBACK(on_btn_get_recommendations_prepare_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_main_stack_recipes_clicked",
+                                    G_CALLBACK(on_btn_main_stack_recipes_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_main_stack_ingredients_clicked",
+                                    G_CALLBACK(on_btn_main_stack_ingredients_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_main_stack_recommendations_clicked",
+                                    G_CALLBACK(on_btn_main_stack_recommendations_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_stack_recommendations_get_recommendations_clicked",
+                                    G_CALLBACK(on_btn_stack_recommendations_get_recommendations_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_stack_recommendations_try_something_new_clicked",
+                                    G_CALLBACK(on_btn_stack_recommendations_try_something_new_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_modify_ingredient_add_clicked",
+                                    G_CALLBACK(on_btn_modify_ingredient_add_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_modify_ingredient_cancel_clicked",
+                                    G_CALLBACK(on_btn_modify_ingredient_cancel_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_add_ingredient_add_clicked",
+                                    G_CALLBACK(on_btn_add_ingredient_add_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_add_ingredient_cancel_clicked",
+                                    G_CALLBACK(on_btn_add_ingredient_cancel_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_ingredients_list_delete_clicked",
+                                    G_CALLBACK(on_btn_ingredients_list_delete_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_ingredients_list_modify_clicked",
+                                    G_CALLBACK(on_btn_ingredients_list_modify_clicked));
+    gtk_builder_add_callback_symbol(builder, "on_btn_ingredients_list_add_clicked",
+                                    G_CALLBACK(on_btn_ingredients_list_add_clicked));
+    gtk_builder_connect_signals(builder, app);
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);
+    gtk_main();
+
+    g_slice_free(App, app);
 }
