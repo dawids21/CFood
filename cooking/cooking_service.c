9fc90c84da45db1cecc23f611d77b593449f57f4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-19 14:21:35 +0200

    add necessary files for cooking module

---
+++ cooking/cooking_service.c
@@ -0,0 +1,5 @@
+//
+// Created by dawids21 on 5/19/21.
+//
+
+#include "cooking_service.h"
---
+++ cooking/cooking_service.h
@@ -0,0 +1,8 @@
+//
+// Created by dawids21 on 5/19/21.
+//
+
+#ifndef CFOOD_COOKING_SERVICE_H
+#define CFOOD_COOKING_SERVICE_H
+
+#endif //CFOOD_COOKING_SERVICE_H
