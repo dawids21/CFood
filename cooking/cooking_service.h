a95dba7cc8037cd444ca146600aee1955b31dad3
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-19 14:27:19 +0200

    declare methods in header file

--- cooking/cooking_service.h
+++ cooking/cooking_service.h
@@ -5,4 +5,8 @@
 #ifndef CFOOD_COOKING_SERVICE_H
 #define CFOOD_COOKING_SERVICE_H
 
+typedef struct CookingService *CookingService;
+
+void cooking_service_prepare(CookingService service, int recipe_id);
+
 #endif //CFOOD_COOKING_SERVICE_H
